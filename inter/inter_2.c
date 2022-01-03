2dc23bc4bd61a1f28c626d11b0c1952e5fb40610
Lucas Lethuillier <lucas.lethuillier@gmail.com>
2022-01-03 01:06:29 +0100

    inter and union, 1 rep each ; ft_printf, 1 rep

---
+++ inter/inter_2.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   inter_2.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lucas <llethuil@student.42lyon.fr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/02 14:05:51 by lucas             #+#    #+#             */
+/*   Updated: 2022/01/02 14:15:10 by lucas            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int main(int argc, char **argv)
+{
+	char	tab[256];
+	char	c;
+	int		i;
+
+	if (argc == 3)
+	{
+		i = -1;
+		while (++i < 256)
+			tab[i] = 0;
+		i = -1;
+		while (argv[2][++i])
+		{
+			c = argv[2][i];
+			if (tab[(unsigned char)c] == 0)
+				tab[(unsigned char)c] = 1;
+		}
+		i = -1;
+		while (argv[1][++i])
+		{
+			c = argv[1][i];
+			if (tab[(unsigned char)c] == 1)
+			{
+				write(1, &c, 1);
+				(tab[(unsigned char)c] = 0);
+			}
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
+}
